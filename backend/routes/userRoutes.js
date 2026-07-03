803f9c9a8c2d8c7bef5421519954b224db02d9be
Harsh Saini <harshsaini5012@gmail.com>
2026-07-03 11:15:10 +0530

    fix photo upload route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -62,6 +62,46 @@
 
 // export default router;
 
+// import express from "express";
+// import {
+//   getUsers,
+//   createUser,
+//   deleteUser,
+//   updateUser,
+//   changePassword,
+// } from "../controllers/userControler.js";
+// import { protect, adminOnly } from "../middleware/authMiddleware.js";
+// import { upload } from "../config/cloudinary.js";
+// import User from "../models/User.js";
+
+// const router = express.Router();
+
+// router.get("/", protect, adminOnly, getUsers);
+// router.post("/", createUser);
+// router.delete("/:id", protect, adminOnly, deleteUser);
+// router.put("/:id", protect, updateUser);
+// router.put("/:id/change-password", protect, changePassword);
+
+// // Upload profile photo
+// router.put(
+//   "/:id/upload-photo",
+//   protect,
+//   upload.single("photo"),
+//   async (req, res) => {
+//     try {
+//       const user = await User.findByIdAndUpdate(
+//         req.params.id,
+//         { photo: req.file.path },
+//         { new: true },
+//       );
+//       res.status(200).json({ photo: user.photo });
+//     } catch (err) {
+//       res.status(500).json({ message: err.message });
+//     }
+//   },
+// );
+
+// export default router;
 import express from "express";
 import {
   getUsers,
@@ -83,22 +123,17 @@ router.put("/:id", protect, updateUser);
 router.put("/:id/change-password", protect, changePassword);
 
 // Upload profile photo
-router.put(
-  "/:id/upload-photo",
-  protect,
-  upload.single("photo"),
-  async (req, res) => {
-    try {
-      const user = await User.findByIdAndUpdate(
-        req.params.id,
-        { photo: req.file.path },
-        { new: true },
-      );
-      res.status(200).json({ photo: user.photo });
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-  },
-);
+router.put("/:id/upload-photo", upload.single("photo"), async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      { photo: req.file.path },
+      { new: true },
+    );
+    res.status(200).json({ photo: user.photo });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 export default router;
